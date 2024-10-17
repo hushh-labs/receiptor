f699bb5aca6152b4f5a9ae565a254aa4c176a308
Omkar Malpure <77787482+omkar806@users.noreply.github.com>
2024-10-17 23:00:18 +0530

    Delete receiptor directory

--- receiptor/models/attachment.py
+++
@@ -1,7 +0,0 @@
-class Attachment:
-    def __init__(self, attachment_len:int,filename: str,attachment_id:str,attachment_raw_text:str):
-        self.attachment_len = attachment_len
-        self.filename = filename
-        # self.data = data
-        self.attachment_id =attachment_id
-        self.attachment_raw_text = attachment_raw_text
